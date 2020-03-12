ccee435b3ea7f111a9453b2cb38983f6e99c14c1
cseshahriar <cse.shahriar.hosen@gmail.com>
2020-03-12 18:06:00 +0600

    django form validation

--- first_project/basicform/forms.py
+++ first_project/basicform/forms.py
@@ -1,42 +1,30 @@
 from django import forms 
+from django.core import validators
 
-class DateInput(forms.DateInput):
+
+class DateInput(forms.DateInput): 
     input_type = 'date' 
 
 
 class FormName(forms.Form):
-    name = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Please enter name here"}), label='Name')    
+    name = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Please enter name here"}), label='Name', required=False)     
     email = forms.EmailField(initial='example@mail.com', help_text='A valid email address, please.')  
     text =   forms.CharField(widget=forms.Textarea(attrs={'rows':'5'}), required=False) 
-    Status = forms.BooleanField() # checkbox 
-    city = forms.ChoiceField(label='City', choices=[
-        ('', "Select Your City"),
-        ('Barishal', "Barisal"),
-        ('Chittagong', "Chittagong"),
-        ('Dhaka', "Dhaka"),
-        ('Khulna', "Khulna"), 
-        ('Mymensingh', "Mymensingh"),
-        ('Rajshahi', "Rajshahi"),
-        ('Rangpur', "Rangpur"),
-        ('Sylhet', "Sylhet")
-    ])  
-    typedChoice = forms.TypedChoiceField(choices=[
-        ('', "Select Your City"),
-        ('Barishal', "Barisal"),
-        ('Chittagong', "Chittagong"),
-        ('Dhaka', "Dhaka"),
-        ('Khulna', "Khulna"), 
-        ('Mymensingh', "Mymensingh"),
-        ('Rajshahi', "Rajshahi"),
-        ('Rangpur', "Rangpur"),
-        ('Sylhet', "Sylhet")
-    ], coerce=str)
-    date = forms.DateField(widget=DateInput) 
-    dateTime = forms.DateTimeField()  
-    price = forms.DecimalField() 
-    duration = forms.DurationField()
-    Image = forms.FileField()
-    ip = forms.GenericIPAddressField()
-    time = forms.TimeField()
-    website = forms.URLField() 
-    slug = forms.SlugField()   
+    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
+ 
+    # form validation 
+    '''
+    def clean_botcatcher(self):
+        botcatcher = self.cleaned_data['botcatcher']
+        if len(botcatcher) > 0:
+            raise forms.ValidationError('GOTCHA BOT!')
+        return botcatcher
+    '''
+
+    def clean(self):
+        all_clean_data = super().clean()  
+        email = all_clean_data['email']
+        name = all_clean_data['name'] 
+
+        if(len(name) <= 0):
+            raise forms.ValidationError('Name field is required!') 
