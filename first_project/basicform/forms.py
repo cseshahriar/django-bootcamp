a61d5d1d4040aaa1ca9ed42f50c4e01845e0c26c
cseshahriar <cse.shahriar.hosen@gmail.com>
2020-03-15 15:47:15 +0600

    user profile

--- first_project/basicform/forms.py
+++ first_project/basicform/forms.py
@@ -1,30 +1,17 @@
-from django import forms 
+from django import forms
+from django.contrib.auth.models import User 
+from basicform.models import UserProfileInfo as UserProfileInfoModel
 from django.core import validators
 
 
-class DateInput(forms.DateInput): 
-    input_type = 'date' 
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
 
+    class Meta():
+        model = User 
+        fields = ('username', 'email', 'password')
 
-class FormName(forms.Form):
-    name = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Please enter name here"}), label='Name', required=False)     
-    email = forms.EmailField(initial='example@mail.com', help_text='A valid email address, please.')  
-    text =   forms.CharField(widget=forms.Textarea(attrs={'rows':'5'}), required=False) 
-    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
- 
-    # form validation 
-    '''
-    def clean_botcatcher(self):
-        botcatcher = self.cleaned_data['botcatcher']
-        if len(botcatcher) > 0:
-            raise forms.ValidationError('GOTCHA BOT!')
-        return botcatcher
-    '''
-
-    def clean(self):
-        all_clean_data = super().clean()  
-        email = all_clean_data['email']
-        name = all_clean_data['name'] 
-
-        if(len(name) <= 0):
-            raise forms.ValidationError('Name field is required!') 
+class UserProfileInfo(forms.ModelForm):
+    class Meta():
+        model = UserProfileInfoModel
+        fields = ('portfolio_site', 'profile_pic') 
